f2de41f5ddca558c26f743f7f4cdc5f062771c54
Semih DEMİRCİ <11303241+smh67dmrc@users.noreply.github.com>
2022-07-25 10:43:04 +0300

    Add files via upload

---
+++ Character-Counter_python3.py
@@ -0,0 +1,16 @@
+# -*- coding: cp1254 -*-
+
+print("Character Counter Tool")
+
+a = input("Input : ")
+def uniq(input):
+  output = []
+  for x in input:
+    if x not in output:
+      output.append(x)
+  return output
+c = uniq(a)
+for b in c:
+    print(b , "=====>" , a.count(b) , "found.")
+
+
